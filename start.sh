cfaa75abe5524cfd11dd1069851367d7ab8cc31c
Nicholas Dinis <dinisusmc@aol.com>
2024-08-01 19:31:14 -0400

    Updated shell

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ sudo apt-get install python3 python3-pip git -y
 git clone https://github.com/dinisusmc/multi_service_automation.git
 cd multi_service_automation/
 sudo pip3 install -r requirements.txt
-sudo python3 backend.py & sudo python todolist.py
+python3 backend.py & python3 todolist.py
